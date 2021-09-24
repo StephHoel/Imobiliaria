e289c0462284e38e3854b2ce88081497f6ff4f0a
StephHoel <steph.hoel@gmail.com>
2021-09-23 23:23:36 -0300

    Criando Enum EstadoCivil e FichaRapida

---
+++ Enum/EstadoCivil.cs
@@ -0,0 +1,12 @@
+namespace Imobiliaria
+{
+    public enum EstadoCivil
+    {
+        Solteiro = 1,
+        Casado = 2,
+        Viuvo = 3,
+        Divorciado = 4,
+        Separado = 5,
+        Uniao_Estavel = 6
+    }
+}
---
+++ Enum/FichaRapida.cs
@@ -0,0 +1,10 @@
+namespace Imobiliaria
+{
+    public enum FichaRapida
+    {
+        NaoEnviada = 1,
+        EmAnalise = 2,
+        Aprovada = 3,
+        Reprovada = 4
+    }
+}
