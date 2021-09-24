d6aff25f0ed4a2ea423eff8a053206975d067d2e
StephHoel <steph.hoel@gmail.com>
2021-09-23 23:24:03 -0300

    Criando Projeto

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Imobiliaria
+{
+   public class Program
+   {
+		static void Main()
+		{
+			/* TODO
+				Menu com:
+					[x]Novo Cliente
+					[]Listar Cliente (baseado em algum critério)
+					[]Editar Cliente (desativar, nunca excluir)
+
+					[]Novo Contrato
+					[]Listar Contrato
+					[]Editar Contrato
+			*/
+
+         Console.Clear();
+			Console.WriteLine("Bem vindx ao Cadastro de Clientes e Contratos");
+			Console.WriteLine("Cadastre seus clientes e seus contratos e não perca mais nada!");
+
+			Output.MenuPrincipal();
+			string opcao = Output.RetornoMenu();
+
+			while (opcao.ToUpper() != "X")
+			{
+				switch (opcao)
+				{
+					case "1": // Novo Cliente
+						Cliente.NovoCliente();
+						break;
+					case "2": // Listar Clientes
+						Arquivo.ListarNaoExcluida();
+						break;
+					case "3": // Editar Cliente
+						Arquivo.ListarExcluida();
+						break;
+					case "4": // Novo Contrato
+						Principal.InserirSerie();
+						break;
+					case "5": // Listar Contratos
+						Principal.AtualizarSerie();
+						break;
+					case "6": // Editar Contrato
+						Principal.ExcluirSerie();
+						break;
+					default:
+						// throw new ArgumentOutOfRangeException();
+						Console.WriteLine("Opção inválida");
+						break;
+				}
+
+				Output.MenuPrincipal();
+				opcao = Output.RetornoMenu();
+			}
+
+			Console.WriteLine(Environment.NewLine + "Obrigada por utilizar nossos serviços. Até a próxima!");
+			Console.WriteLine("Pressione ENTER para sair");
+			Console.ReadLine();
+		}
+   }
+}
