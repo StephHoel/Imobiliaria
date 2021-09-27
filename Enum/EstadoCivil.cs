727b290d01e8983ef19801cfc88ac6f888f4178a
StephHoel <steph.hoel@gmail.com>
2021-09-27 00:01:45 -0300

    Update EstadoCivil.cs
    Trocando a formatação

--- Enum/EstadoCivil.cs
+++ Enum/EstadoCivil.cs
@@ -1,16 +1,13 @@
-using System.ComponentModel;
-
 namespace Imobiliaria
 {
    public enum EstadoCivil
    {
-      Solteiro = 1,
-      Casado = 2,
-      Viúvo = 3,
-      Divorciado = 4,
-      Separado = 5,
-
-      União_Estável = 6
+      Solteiro,
+      Casado,
+      Viúvo,
+      Divorciado,
+      Separado,
+      União_Estável
 
     }
 }
