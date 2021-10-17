358c3974452fa38fb0fd06101ff648792ae663e7
StephHoel <steph.hoel@gmail.com>
2021-10-16 23:57:42 -0300

    Add Metodos de Criação de Listas

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using Imobiliaria.Classes;
-using Imobiliaria.Interfaces;
 using Imobiliaria.Methods;
 
 namespace Imobiliaria
@@ -11,77 +10,13 @@ namespace Imobiliaria
    {
       static void Main()
       {
+         List<Contrato> contrato = ColetaContratos();
+         List<Endereco> endereco = ColetaEnderecos();
+         List<Imovel> imovel = ColetaImoveis();
+         List<Pessoa> pessoa = ColetaPessoas();
+         List<Telefone> telefone = ColetaTelefones();
+         List<Usuario> usuario = ColetaUsuarios();
 
-			// Criar um void para criar as listas
-			List<Contrato> contrato = new();
-         string[] readContrato = File.ReadAllLines("DataBase/contrato.db");
-         if (readContrato.Length != 0)
-         {
-            foreach (string line in readContrato)
-            {
-               string[] l = Output.Split(line);
-
-               Contrato contrato1 = new(contrato.Count, Encriptografia.Decrypt(l[1]), int.Parse(l[2]), l[3], l[4], double.Parse(l[5]), int.Parse(l[6]), bool.Parse(l[7]), bool.Parse(l[8]));
-               contrato.Add(contrato1);
-            }
-         }
-
-         List<Endereco> endereco = new();
-         string[] readEndereco = File.ReadAllLines("DataBase/endereco.db");
-         if (readEndereco.Length != 0)
-         {
-            foreach (string line in readEndereco)
-            {
-               string[] l = Output.Split(line);
-
-               Endereco endereco1 = new(endereco.Count, Encriptografia.Decrypt(l[1]), int.Parse(l[2]), l[3], int.Parse(l[4]), l[5], l[6], l[7], l[8], l[9], bool.Parse(l[10]));
-               endereco.Add(endereco1);
-            }
-         }
-
-         List<Imovel> imovel = new();
-         string[] readImovel = File.ReadAllLines("DataBase/imovel.db");
-         if (readImovel.Length != 0)
-         {
-            foreach (string line in readImovel)
-            {
-               string[] l = Output.Split(line);
-
-               Imovel imovel1 = new(imovel.Count, Encriptografia.Decrypt(l[1]), l[2], int.Parse(l[3]), l[4], int.Parse(l[5]), l[6], l[7], l[8], l[9], l[10], bool.Parse(l[11]));
-               imovel.Add(imovel1);
-            }
-         }
-
-         List<Pessoa> pessoa = new();
-         string[] readPessoa = File.ReadAllLines("DataBase/pessoa.db");
-         if (readPessoa.Length != 0)
-         {
-            foreach (string line in readPessoa)
-            {
-               string[] l = Output.Split(line);
-
-               // Console.WriteLine(l[0]);
-               // Console.WriteLine(Encriptografia.Decrypt(l[0]));
-               string cpf = Encriptografia.Decrypt(l[1]);
-               string rg = Encriptografia.Decrypt(l[3]);
-
-               Pessoa cliente = new(pessoa.Count, cpf, l[2], rg, l[4], l[5], l[6], l[7], l[8], l[9], l[10], l[11], l[12]);
-               pessoa.Add(cliente);
-            }
-         }
-
-         List<Telefone> telefone = new();
-         string[] readTelefone = File.ReadAllLines("DataBase/telefone.db");
-         if (readTelefone.Length != 0)
-         {
-            foreach (string line in readTelefone)
-            {
-               string[] l = Output.Split(line);
-
-               Telefone tel = new(telefone.Count, Encriptografia.Decrypt(l[1]), int.Parse(l[2]), int.Parse(l[3]), bool.Parse(l[4]), bool.Parse(l[5]), bool.Parse(l[6]));
-               telefone.Add(tel);
-            }
-         }
 
 			/* TODO
 				Menu com:
@@ -106,6 +41,11 @@ namespace Imobiliaria
 			Console.WriteLine("Cadastre seus clientes e seus contratos e não perca mais nada!");
 			string opcao;
 
+			Console.Write("Usuário: ");
+         Console.ReadLine();
+         Console.Write("Senha: ");
+         Console.ReadLine();
+
 			do
 			{
             Output.Titulo("Menu Principal");
@@ -122,10 +62,10 @@ namespace Imobiliaria
 				switch (opcao)
 				{
 					case "1": // Novo Cliente
-						Cliente.NovoCliente(pessoa.Count);
+                  Cliente.NovoCliente();
 						break;
 					case "2": // Listar Clientes
-                  Cliente.ListarCliente(pessoa);
+                  Cliente.ListarCliente(pessoa, telefone);
 						break;
 					case "3": // Editar Cliente
                   Cliente.AtualizaCliente(pessoa);
@@ -154,5 +94,112 @@ namespace Imobiliaria
 			Console.WriteLine("Pressione ENTER para sair");
 			Console.ReadLine();
 		}
-	}
+
+      private static List<Contrato> ColetaContratos()
+      {
+         List<Contrato> contratos = new();
+         string[] readContrato = File.ReadAllLines("DataBase/contrato.db");
+         if (readContrato.Length != 0)
+         {
+            foreach (string line in readContrato)
+            {
+               string[] l = Output.Split(line);
+
+               Contrato contrato1 = new(contratos.Count, Encriptografia.Decrypt(l[1]), int.Parse(l[2]), l[3], l[4], double.Parse(l[5]), int.Parse(l[6]), bool.Parse(l[7]), bool.Parse(l[8]));
+               contratos.Add(contrato1);
+            }
+         }
+         return contratos;
+      }
+
+      private static List<Endereco> ColetaEnderecos()
+      {
+         List<Endereco> lista = new();
+         string[] readEndereco = File.ReadAllLines("DataBase/endereco.db");
+         if (readEndereco.Length != 0)
+         {
+            foreach (string line in readEndereco)
+            {
+               string[] l = Output.Split(line);
+
+               Endereco endereco1 = new(lista.Count, Encriptografia.Decrypt(l[1]), int.Parse(l[2]), l[3], int.Parse(l[4]), l[5], l[6], l[7], l[8], l[9], bool.Parse(l[10]));
+               lista.Add(endereco1);
+            }
+         }
+         return lista;
+      }
+
+      private static List<Imovel> ColetaImoveis()
+      {
+         List<Imovel> lista = new();
+         string[] readImovel = File.ReadAllLines("DataBase/imovel.db");
+         if (readImovel.Length != 0)
+         {
+            foreach (string line in readImovel)
+            {
+               string[] l = Output.Split(line);
+
+               Imovel imovel1 = new(lista.Count, Encriptografia.Decrypt(l[1]), l[2], int.Parse(l[3]), l[4], int.Parse(l[5]), l[6], l[7], l[8], l[9], l[10], bool.Parse(l[11]));
+               lista.Add(imovel1);
+            }
+         }
+         return lista;
+      }
+
+      private static List<Pessoa> ColetaPessoas()
+      {
+         List<Pessoa> lista = new();
+         string[] readPessoa = File.ReadAllLines("DataBase/pessoa.db");
+         if (readPessoa.Length != 0)
+         {
+            foreach (string line in readPessoa)
+            {
+               string[] l = Output.Split(line);
+
+               // Console.WriteLine(l[0]);
+               // Console.WriteLine(Encriptografia.Decrypt(l[0]));
+               string cpf = Encriptografia.Decrypt(l[1]);
+               string rg = Encriptografia.Decrypt(l[3]);
+
+               Pessoa cliente = new(lista.Count, cpf, l[2], rg, l[4], l[5], l[6], l[7], l[8], l[9], l[10], l[11], l[12]);
+               lista.Add(cliente);
+            }
+         }
+         return lista;
+      }
+
+      private static List<Telefone> ColetaTelefones()
+      {
+         List<Telefone> lista = new();
+         string[] readTelefone = File.ReadAllLines("DataBase/telefone.db");
+         if (readTelefone.Length != 0)
+         {
+            foreach (string line in readTelefone)
+            {
+               string[] l = Output.Split(line);
+
+               Telefone tel = new(lista.Count, Encriptografia.Decrypt(l[1]), int.Parse(l[2]), int.Parse(l[3]), bool.Parse(l[4]), bool.Parse(l[5]), bool.Parse(l[6]));
+               lista.Add(tel);
+            }
+         }
+         return lista;
+      }
+
+      private static List<Usuario> ColetaUsuarios()
+      {
+         List<Usuario> lista = new();
+         string[] readEndereco = File.ReadAllLines("DataBase/usuario.db");
+         if (readEndereco.Length != 0)
+         {
+            foreach (string line in readEndereco)
+            {
+               string[] l = Output.Split(line);
+
+               // Usuario endereco1 = new(lista.Count, Encriptografia.Decrypt(l[1]), int.Parse(l[2]), l[3], int.Parse(l[4]), l[5], l[6], l[7], l[8], l[9], bool.Parse(l[10]));
+               // lista.Add(endereco1);
+            }
+         }
+         return lista;
+      }
+   }
 }
