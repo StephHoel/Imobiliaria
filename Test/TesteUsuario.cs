d8797d2474cd86a2f698c3506ccad13df8f3d432
StephHoel <steph.hoel@gmail.com>
2021-11-03 01:52:55 -0300

    Add Project Test

---
+++ Test/TesteUsuario.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Imobiliaria.Test
+{
+   [TestClass]
+   public class TesteUsuario
+   {
+      [TestMethod]
+      public void TestMethod1()
+      {
+
+      }
+   }
+}
